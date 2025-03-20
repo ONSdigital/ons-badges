e8f17227a125f558ff514fe85c14d67dec8030b7
Angus <angus.goody@ons.gov.uk>
2025-03-20 14:33:46 +0000

    new ui

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,145 +1,131 @@
 <template>
-  <div class="text-gray-100 h-screen p-6 bg-gray-800">
-    <div class="grid grid-cols-4 gap-2 w-full">
-      <!-- Controls -->
-      <div class="col-span-1 p-4 ">
-        <!-- Controls -->
-        <div class="col-span-1 p-4">
-          <h3 class="text-xl mb-4">Enter Text for Left and Right</h3>
-          <div>
-              <label for="left_text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Left text (width: {{leftShift}})</label>
-              <input v-model="leftText" type="text" id="left_text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="A" required />
-          </div>
-          <div class="mt-6">
-              <label for="right_text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Right text (width: {{rightShift}})</label>
-              <input v-model="rightText" type="text" id="right_text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="B" required />
-          </div>
-          <hr class="my-10">
-          <h3 class="text-xl mb-4">Colours</h3>
+    <div class="h-full bg-gray-200 dark:bg-gray-900">
+        <div class="container mx-auto p-10">
+            <header>
+                <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-[#a8bd3a] from-[#f0f762]">ONS</span> Badges</h1>
+                <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Generate dynamic ONS themed badges for README's</p>
+            </header>
 
+            <main class="py-20">
+                <div class="grid grid-cols-10 gap-10">
+                    <div class="col-span-6 ">
 
-          <div v-for="(color, index) in colorOptions" :key="index" class="flex items-center mb-4">
-            <input type="radio" :id="'color-' + index" v-model="selectedColor" :value="color" class="radio">
-            <label :for="'color-' + index" class="ms-2 text-sm font-medium" :style="{color: color.fg}">{{ color.name }}</label>
-          </div>
+                        <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Generate a preview</h2>
+                        <div class="bg-gray-100 dark:bg-gray-800 rounded-lg p-8">
+                            <form>
+                                <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                                    <div class="w-full">
+                                        <label for="left_text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Left Text</label>
+                                        <input type="text" name="left_text" id="left_text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required="">
+                                    </div>
+                                    <div class="w-full">
+                                        <label for="right_text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Right Text</label>
+                                        <input type="text" name="brand" id="brand" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required="">
+                                    </div>
 
-
-
-
-        </div>
-      </div>
-
-      <!-- SVG Preview -->
-      <div class="col-span-3 bg-white flex justify-center items-center">
-        <div class="bg-white">
-          <svg :width="newGlobalWidth" height="312px" xmlns="http://www.w3.org/2000/svg">
-              <g id="production-pill">
-                  <g id="middle">
-                      <rect id="background" :x="newBackground" y="0" width="211.717" height="312" style="fill:#fff;"/>
-                  </g>
-                  <g id="left">
-                      <path id="left-shape" :d="newLeft" style="fill:#a8bd3a;"/>
-                      <g id="left-text-section">
-                        <text x="60.727px" y="219.003px" style="font-family: monospace; font-weight:600;font-size:176.512px;fill:#000000;">
-                          {{leftText}}
-                        </text>
-                      </g>
-                  </g>
-                  <g id="right">
-                      <path id="right-shape" :d="newRight" :fill="selectedColor.bg"/>
-                      <g id="right-text-section">
-                          <text :x="newRightTextPos" y="219.003px" style="font-family: monospace; font-weight:600;font-size:176.512px;" :fill="selectedColor.fg">
-                            {{rightText}}
-                          </text>
-                      </g>
-                  </g>
-              </g>
-          </svg>
+                                    <div class="sm:col-span-2">
+                                        <ColourSelect/>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-span-4 ">
+                        <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">SVG Preview</h2>
+                        <div class="bg-gray-100 dark:bg-gray-800 rounded-lg p-8">
+                            <div class="bg-white rounded-lg p-8">
+                                SVG
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
 
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
+import ColourSelect from "@/ColourSelect.vue";
+
 export default {
-  data() {
-    return {
+    components: {ColourSelect},
+    data() {
+        return {
 
-      // Store text
-      leftText: "A",
-      rightText: "B",
+            // Store text
+            leftText: "A",
+            rightText: "B",
 
-      charWidth: 109.283,
+            charWidth: 109.283,
 
-      // Orignal widths
-      originalGlobalWidth: 360, //Pixels
-      selectedColor: { name: "Default", bg: "#013B61", fg: "#ffffff" },
-      colorOptions: [
-        { name: "Default", bg: "#013B61", fg: "#ffffff" },
-        { name: "Error", bg: "#D0021B", fg: "#ffffff" },
-        { name: "Success", bg: "#0F8243", fg: "#ffffff" },
-        { name: "Warning", bg: "#FA6401", fg: "000000" },
-        { name: "Gray", bg: "#414042", fg: "#ffffff" }
-      ]
+            // Orignal widths
+            originalGlobalWidth: 360, //Pixels
+            selectedColor: { name: "Default", bg: "#013B61", fg: "#ffffff" },
+            colorOptions: [
+                { name: "Default", bg: "#013B61", fg: "#ffffff" },
+                { name: "Error", bg: "#D0021B", fg: "#ffffff" },
+                { name: "Success", bg: "#0F8243", fg: "#ffffff" },
+                { name: "Warning", bg: "#FA6401", fg: "000000" },
+                { name: "Gray", bg: "#414042", fg: "#ffffff" }
+            ]
 
-    };
-  },
-  computed: {
-    getTextWidth() {
-        return (text, font = "176.512px 'monospace'") => {
-          return text.length * this.charWidth;
         };
-      },
-
-    leftShift() {
-      if (!this.leftText){
-        return 0;
-      }
-      return parseInt(this.getTextWidth(this.leftText), 10);
     },
-    rightShift() {
-      if (!this.rightText){
-        return 0;
-      }
-      return parseInt(this.getTextWidth(this.rightText), 10);
-    },
+    computed: {
+        getTextWidth() {
+            return (text, font = "176.512px 'monospace'") => {
+                return text.length * this.charWidth;
+            };
+        },
 
-    newBackground()
-    {
-      const newPos = 74 + this.leftShift;
-      return newPos;
-    },
+        leftShift() {
+            if (!this.leftText){
+                return 0;
+            }
+            return parseInt(this.getTextWidth(this.leftText), 10);
+        },
+        rightShift() {
+            if (!this.rightText){
+                return 0;
+            }
+            return parseInt(this.getTextWidth(this.rightText), 10);
+        },
 
-    newLeft(){
-      const newTop = 171.876 + this.leftShift;
-      const newBottom = 9.039 + this.leftShift;
-      const newStart = 74 + this.leftShift;
-      return `M${newStart},312l-${newBottom},0c-35.877,0 -64.961,-29.084 -64.961,-64.961l0,-182.078c0,-35.877 29.084,-64.961 64.961,-64.961l${newTop},0l-162.837,312Z`;
-    },
+        newBackground()
+        {
+            const newPos = 74 + this.leftShift;
+            return newPos;
+        },
 
-    newRight()
-    {
-      const newTop = 9.039 + this.rightShift;
-      const newBottom = 171.877 + this.rightShift;
-      const newStart = 285.717 + this.leftShift;
-      return `M${newStart},0l${newTop},0c35.877,-0 64.961,29.084 64.961,64.961l-0,182.078c-0,35.877 -29.084,64.961 -64.961,64.961l-${newBottom},0l162.838,-312Z`;
-    },
+        newLeft(){
+            const newTop = 171.876 + this.leftShift;
+            const newBottom = 9.039 + this.leftShift;
+            const newStart = 74 + this.leftShift;
+            return `M${newStart},312l-${newBottom},0c-35.877,0 -64.961,-29.084 -64.961,-64.961l0,-182.078c0,-35.877 29.084,-64.961 64.961,-64.961l${newTop},0l-162.837,312Z`;
+        },
 
-    newRightTextPos()
-    {
-      const originalPos = 272.444;
-      return originalPos + this.leftShift;
-    },
+        newRight()
+        {
+            const newTop = 9.039 + this.rightShift;
+            const newBottom = 171.877 + this.rightShift;
+            const newStart = 285.717 + this.leftShift;
+            return `M${newStart},0l${newTop},0c35.877,-0 64.961,29.084 64.961,64.961l-0,182.078c-0,35.877 -29.084,64.961 -64.961,64.961l-${newBottom},0l162.838,-312Z`;
+        },
 
-    newGlobalWidth()
-    {
-      return this.originalGlobalWidth + this.leftShift + this.rightShift;
-    },
+        newRightTextPos()
+        {
+            const originalPos = 272.444;
+            return originalPos + this.leftShift;
+        },
+
+        newGlobalWidth()
+        {
+            return this.originalGlobalWidth + this.leftShift + this.rightShift;
+        },
 
 
-  }
+    }
 };
 </script>
 
---
+++ frontend/src/ColourSelect.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="bg-gray-100 rounded-lg p-4">
+        <fieldset>
+            <legend class="block mb-2 text-sm font-medium text-gray-900">Choose a colour</legend>
+            <RadioGroup v-model="colour" class="mt-6 flex items-center gap-x-3">
+
+                <div class="flex flex-col items-center gap-y-1">
+                    <RadioGroupOption v-slot="{ checked }" value="default">
+                        <label
+                            aria-label="default"
+                            :class="[
+                    'relative flex cursor-pointer items-center justify-center rounded-full p-1 text-[#003c57] ring-current focus:outline-hidden',
+                      checked && 'ring-3 ring-offset-1'
+                ]"
+                        >
+                            <input type="radio" name="color-choice" value="Pink" class="sr-only">
+                            <span aria-hidden="true" class="size-8 rounded-full border border-black/10 bg-current"></span>
+                        </label>
+                    </RadioGroupOption>
+                    <small class="text-gray-900  text-center w-full">Default</small>
+                </div>
+
+                <div class="flex flex-col items-center gap-y-1">
+                    <RadioGroupOption v-slot="{ checked }" value="danger">
+                        <label
+                            aria-label="Danger"
+                            :class="[
+                    'relative flex cursor-pointer items-center justify-center rounded-full p-1 text-[#d0021b] ring-current focus:outline-hidden',
+                      checked && 'ring-3 ring-offset-1'
+                ]"
+                        >
+                            <input type="radio" name="color-choice" value="Pink" class="sr-only">
+                            <span aria-hidden="true" class="size-8 rounded-full border border-black/10 bg-current"></span>
+                        </label>
+                    </RadioGroupOption>
+                    <small class="text-gray-900  text-center w-full">Danger</small>
+                </div>
+
+                <div class="flex flex-col items-center gap-y-1">
+                    <RadioGroupOption v-slot="{ checked }" value="success">
+                        <label
+                            aria-label="Success"
+                            :class="[
+                    'relative flex cursor-pointer items-center justify-center rounded-full p-1 text-[#0f8243] ring-current focus:outline-hidden',
+                      checked && 'ring-3 ring-offset-1'
+                ]"
+                        >
+                            <input type="radio" name="color-choice" value="Pink" class="sr-only">
+                            <span aria-hidden="true" class="size-8 rounded-full border border-black/10 bg-current"></span>
+                        </label>
+                    </RadioGroupOption>
+                    <small class="text-gray-900  text-center w-full">Success</small>
+                </div>
+
+                <div class="flex flex-col items-center gap-y-1">
+                    <RadioGroupOption v-slot="{ checked }" value="warning">
+                        <label
+                            aria-label="Warning"
+                            :class="[
+                    'relative flex cursor-pointer items-center justify-center rounded-full p-1 text-[#fa6401] ring-current focus:outline-hidden',
+                      checked && 'ring-3 ring-offset-1'
+                ]"
+                        >
+                            <input type="radio" name="color-choice" value="Pink" class="sr-only">
+                            <span aria-hidden="true" class="size-8 rounded-full border border-black/10 bg-current"></span>
+                        </label>
+                    </RadioGroupOption>
+                    <small class="text-gray-900  text-center w-full">Warning</small>
+                </div>
+            </RadioGroup>
+        </fieldset>
+    </div>
+
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  import {
+    RadioGroup,
+    RadioGroupLabel,
+    RadioGroupOption,
+  } from '@headlessui/vue'
+
+  const colour = ref('default')
+</script>
